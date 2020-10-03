6f6a5f1d3767e4134798f78e681601819b4e341b
Jake Callcut <jakecallcut123@gmail.com>
2020-10-03 13:33:59 +0100

    continued ws12

---
+++ Encryption/Processing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Encryption
+{
+    class Processing
+    {
+        public static int AppplyShift(string input, int shift)
+        {
+            int y;                                                                              //last edit
+            for (int i = 0; i < input.Length; i++)
+            {
+                char x = Convert.ToChar(input.Substring(0, 1));
+                y = (int)x;
+            }
+
+            return y;
+            
+        }
+    }
+}
--- Encryption/Program.cs
+++ Encryption/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Encryption
 {
@@ -6,9 +7,29 @@ namespace Encryption
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Would you like to encrypt or decrypt text? (E/D)");
+            string encOrDec = Console.ReadLine();
+            Console.Clear();
 
-           
+            if (encOrDec.ToUpper() == "E")
+            {
+                Console.WriteLine("Please enter the line to encrypt");
+                string lineToEncrypt = Console.ReadLine();
 
+                Console.WriteLine("Please enter the number by which you would like to shift the text by");
+                int shiftNumber = Convert.ToInt32(Console.ReadLine());
+
+                Processing.AppplyShift(lineToEncrypt, shiftNumber);                         //last edit
+
+            }
+            else if (encOrDec.ToUpper() == "D")
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter either E or D, to encrypt or decrypt");
+            }
         }
     }
 }
